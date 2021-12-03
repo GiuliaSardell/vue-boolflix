cca9d2a7c28a5fbc95801b02eda0f328131bd272
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-12-03 17:05:48 +0100

    inserimento flip card e trama

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,93 +1,141 @@
 <template>
   <main>
 
-    <!-- voglio mettere film e serie visibili quando una variabile booleana è vera
-    devo mettere la variabile = false, diventa true se l'array non è vuoto-->
+
+      
+
+
+        
+      
+
+        
+      
+
     <div class="films" >
       <h2 class="my-5" v-if="fullArrayFilm">FILM</h2> 
 
       <div v-for="film in filmList"
       :key="film.id">
-      
-      <h4>Titolo: {{film.title}}</h4>
-      <h6>Titolo originale: {{film.original_title}}</h6>
-      <img
-      :src="`https://image.tmdb.org/t/p/w342/${film.poster_path}`" 
-      :alt="film.title">
-     
 
-      <!-- <h6>Lingua: {{film.original_language}}</h6> -->
-      <!-- <h6 v-if="film.original_language === 'en'">
-        Lingua: <img src="../assets/img/it.png" alt="">
-      </h6> -->
-      <!-- <h6 v-if="langArray.includes('en')">
-        Lingua: <img  :src="`../assets/img/${film.original_language}.png`" :alt="`${film.original_language}`">
-      </h6>
-      <h6 v-else>
-        Lingua: {{film.original_language}}
-      </h6> -->
-      <div>
-        <p v-if="langArray.includes(film.original_language)"> Lingua: 
-          <img class="flag"
-          :src="require(`../assets/img/${film.original_language}.png`)" :alt="film.title">
-        </p>
+        <div class="scene scene--card">
+          <div
+          class="card"
+          @click="cardOne == 'start' ? (cardOne = 'flipped' ) : (cardOne = 'start' )"
+          v-bind:class="{ flipme: cardOne == 'flipped' }"
+          >
+
+
+            <div class="card__face card__face--front">
         
-        <p v-else>Lingua: {{film.original_language}}</p>
-      </div>
-      
-      <!-- <h6>Voto: {{film.vote_average}} -> {{math.round(film.vote_average/2)}} </h6>  -->
-      <h6>Voto: {{Math.round(film.vote_average/2)}}</h6>
+              <h4>Titolo: {{film.title}}</h4>
+              <h6>Titolo originale: {{film.original_title}}</h6>
+              <img
+              :src="`https://image.tmdb.org/t/p/w342/${film.poster_path}`" 
+              :alt="film.title">
+            
+              <div>
+                <p v-if="langArray.includes(film.original_language)"> Lingua: 
+                  <img class="flag"
+                  :src="require(`../assets/img/${film.original_language}.png`)" :alt="film.title">
+                </p>
+                
+                <p v-else>Lingua: {{film.original_language}}</p>
+              </div>
+          
+              <div>
+                <p>
 
-  
-      <div>
-        <i
-        v-for="(item, index) in 5" :key="index"
-        class="fa-star"
-        :class="index < Math.round(film.vote_average/2) ? 'fas' : 'far' ">
-        </i>
-       
-  
-      </div>
+                  Voto: 
+                  <i
+                  v-for="(item, index) in 5" :key="index"
+                  class="fa-star"
+                  :class="index < Math.round(film.vote_average/2) ? 'fas' : 'far' ">
+                </i>
+              
+                </p>
+                
+              </div>
+            </div>
+
+            <div class="card__face card__face--back">Trama: {{film.overview}}</div>
 
 
-    
+          </div>
+        </div>
       </div>
     </div>
     
 
+
+
+
+
+
+
+
+
+
+
     <div class="series justify-content-center">
 
-      <h2 class="my-5 container">SERIE</h2> 
+      <h2 class="my-5 container" v-if="fullArraySerie">SERIE</h2> 
 
       <div class=""
       v-for="serie in serieList"
       :key="serie.id"> 
-      
-        <h4>Titolo: {{serie.name}}</h4>
-        <h6>Titolo originale: {{serie.original_name}}</h6>
-        <!-- <h6>Lingua: {{film.original_language}}</h6> -->
-        <h6 v-if="serie.original_language === 'en'">
-          Lingua: <img src="../assets/img/it.png" alt="">
-        </h6>
-        <h6 v-else-if="serie.original_language === 'it'">
-          Lingua: <img src="../assets/img/en.png" alt="">
-        </h6>
-        <h6 v-else>
-          Lingua: {{serie.original_language}}
-        </h6>
-        <h6>Voto: {{serie.vote_average}}</h6>
-    
+
+        <div class="scene scene--card">
+          <div
+          class="card"
+          @click="cardOne == 'start' ? (cardOne = 'flipped' ) : (cardOne = 'start' )"
+          v-bind:class="{ flipme: cardOne == 'flipped' }"
+          >
+
+
+            <div class="card__face card__face--front">
+        
+              <h4>Titolo: {{serie.name}}</h4>
+              <h6>Titolo originale: {{serie.original_name}}</h6>
+              <img
+              :src="`https://image.tmdb.org/t/p/w342/${serie.poster_path}`" 
+              :alt="serie.title">
+
+                <div>
+                  <p v-if="langArray.includes(serie.original_language)"> Lingua: 
+                    <img class="flag"
+                    :src="require(`../assets/img/${serie.original_language}.png`)" :alt="serie.name">
+                  </p>
+                
+                  <p v-else>Lingua: {{serie.original_language}}</p>
+                </div>
+
+
+
+                <div>
+                <p>
+                  Voto: 
+                  <i
+                  v-for="(item, index) in 5" :key="index"
+                  class="fa-star"
+                  :class="index < Math.round(serie.vote_average/2) ? 'fas' : 'far' ">
+                  </i>
+                </p>
+                </div>
+                
+            </div>
+            <div class="card__face card__face--back">Trama: {{serie.overview}}</div>
+
+          </div>
+        </div>
       </div>
     </div>
     
-    <Card />
   </main>
 </template>
 
 <script>
 
 //IMPORT COMPONENTS
-import Card from "./Card.vue"
 
 export default {
   name: 'Main',
@@ -97,12 +145,11 @@ export default {
     fullArrayFilm: Boolean,
     fullArraySerie: Boolean
   },
-  components:{
-    Card
-  },
+
   data(){
     return{
-      langArray: ['en', 'it']
+      langArray: ['en', 'it'],
+      cardOne: 'start'
       
     }
   }
@@ -113,17 +160,56 @@ export default {
 
 .series, .films{
   margin: 20px 10px;
-  h4{
-    margin-top: 50px;
-  }
+
   .flag{
     height: 20px;
     width: 30px;
   }
   img{
     width: 200px;
-    margin: 10px 0;
   }
+}
+
+
+
+.scene {
+  width: 300px;
+  height: 450px;
+  border: 1px solid #ccc;
+  margin: 40px;
+  text-align: center;
+  // padding: 20px;
+  // perspective: 600px;
+}
+
+.card {
+  width: 100%;
+  height: 100%;
+  transition: transform 1s;
+  transform-style: preserve-3d;
+  cursor: pointer;
+  position: relative;
+}
+
+.card__face {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  color: black;
+
+
+  backface-visibility: hidden;
+}
+
+
+.card__face--back {
+  transform: rotateY(180deg);
+}
+
+/* this style is applied when the card is clicked */
+.flipme {
+  transform: rotateY(180deg);
 }
 
 </style>
